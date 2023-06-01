ec484f304ef51a84d12010d795d86137239a4ab1
DanilWork <bogdanovdanila1404@gmail.com>
2023-06-01 12:14:04 +0500

    feat: implement memoize

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -88,8 +88,16 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+
+  return () => {
+    if (cache) {
+      return cache;
+    }
+    cache = func();
+    return cache;
+  };
 }
 
 
